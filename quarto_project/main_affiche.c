da55b2b6a9d2cff822b0fb05f4d7a0b9ba6c5f2a
Frixiol <79292185+Frixiol@users.noreply.github.com>
2021-12-05 00:53:14 +0100

    Add files via upload

--- quarto_project/main_affiche.c
+++ quarto_project/main_affiche.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 #include "board.h"
 
-/**
- * @file main_example.c
- * @brief a simple illustration of how to include and use board.h and board.o.
- * @author Dorbec
- */
 
+// creation d'une instance de board
+board game;
+
+// variable des characteristique des pieces
 enum size piece_taille;
 enum shape piece_forme;
 enum color piece_couleur;
@@ -17,46 +16,218 @@ enum top piece_interieur;
 int piece_x;
 int piece_y;
 
+// creation d'un tableau des pieces restante
+int ava_piece[16][4];
+
+// creation d'un tableau permettant l'affichage des piece sur le plateau
+int played_piece[16] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ,-1};
+
+
+/* permet de remettre toute les pieces dans le tableau 
+ d'affichage des pieces disponible */
+void reset_available_piece()
+{
+	
+	for (int i = 0; i < 2; i++) 
+	{
+		for (int y = 0; y < 2; y++) 
+		{
+			for (int z = 0; z < 2; z++) 
+			{
+				for (int t = 0; t < 2; t++) 
+				{
+					ava_piece[i*8 + y*4 + z*2 + t][0] = i;
+					ava_piece[i*8 + y*4 + z*2 + t][1] = y;
+					ava_piece[i*8 + y*4 + z*2 + t][2] = z;
+					ava_piece[i*8 + y*4 + z*2 + t][3] = t;			
+				 }
+			}
+		}
+	}
+	
+}
+
+// affichage du titre Principal
+void titre()
+{
+	
+	system("clear");
+	for (int i = 0; i < 5; i++)
+	{
+		for (int y = 0; y < 40; y++)
+		{
+			
+			if (i == 0 || i == 4) 
+			{
+				printf("\033[9m \033[m");
+			}
+			
+			else 
+			{
+				if (y == 0 || y == 39)
+				{
+					printf("|");
+				}
+				
+				else 
+				{
+					if (i == 2 && y == 13) 
+					{	
+						printf("PROJECT-QUARTO");
+						y += 13;
+					}
+					
+					else 
+					{
+						printf("\033[95m \033[m");
+					}
+				}
+			}
+			
+		}
+		printf("\n");
+	}
+}
+
+// permet d'afficher les symbole representent les pieces 
+void print_symbol(int a)
+{
+	
+	switch (a)
+	{
+		case 0:
+			printf("\033[31m□\033[m");
+			break;
+		case 1:
+			printf("\033[31m■\033[m");
+			break;
+		case 2:
+			printf("\033[34m□\033[m");
+			break;
+		case 3:
+			printf("\033[34m■\033[m");
+			break;
+		case 4:
+			printf("\033[31m◯\033[m");
+			break;
+		case 5:
+			printf("\033[31m⬤\033[m");
+			break;
+		case 6:
+			printf("\033[34m◯\033[m");
+			break;
+		case 7:
+			printf("\033[34m⬤\033[m");
+			break;
+		case 8:
+			printf("\033[31m▫\033[m");
+			break;
+		case 9:
+			printf("\033[31m▪\033[m");
+			break;
+		case 10:
+			printf("\033[34m▫\033[m");
+			break;
+		case 11:
+			printf("\033[34m▪\033[m");
+			break;
+		case 12:
+			printf("\033[31m○\033[m");
+			break;
+		case 13:
+			printf("\033[31m●\033[m");
+			break;
+		case 14:
+			printf("\033[34m○\033[m");
+			break;
+		case 15:
+			printf("\033[34m●\033[m");
+			break;
+		default:
+			printf(" ");
+			break;
+	}						
+								
+}
 
 // fonction permettant de demander aux joueur de choisir une pièce
 // en fonction de c'est characteristique, 0 ou 1
 piece choose_piece(int nb_tours)
 {
 
+	int a = 2;
+	int b = 2; 
+	int c = 2;
+	int d = 2;
+	
 	if (nb_tours % 2 == 0)
 	{
-		printf("joueur 1 choisiser une piece:");
+		printf("joueur 1 choisiser une piece");
 	}
 	else
 	{
-		printf("joueur 2 choisiser une piece:");
+		printf("joueur 2 choisiser une piece");
 	}
+	printf("\nveuiller entrer 4 nombre de 0 ou 1:");
+	
+	// permet de n'ecrire que 4 int
+	while (!((a == 0 || a == 1) && (b == 0 || b == 1) && (c == 0 || c == 1) && (d == 0 || d == 1)))
+	{
+		scanf("%1d%1d%1d%1d", &a, &b, &c, &d);
 
-
-	scanf("%u", &piece_taille);
-	scanf("%u", &piece_forme);
-	scanf("%u", &piece_couleur);
-	scanf("%u", &piece_interieur);
-
+		printf("\nveuiller reesayer avec les bonne valeur\n");
+		printf("veuiller entrer 4 nombre de 0 ou 1:");
+		while ((getchar()) != '\n');
+	}
+	
+	piece_taille = a;
+	piece_forme = b;
+	piece_couleur = c;
+	piece_interieur = d;
 
 	return get_piece_from_characteristics(piece_taille, piece_forme, piece_couleur, piece_interieur);
 
 }
 
 // fonction permettant aux joueur de plaçer la piece qui fut choisie
-void choose_emplacement(int nb_tours)
+void choose_emplacement(int nb_tours, piece p)
 {
+	int a = 4;
+	int b = 4; 
+	
 	if (nb_tours % 2 == 0)
-		{
-			printf("joueur 2 choisiser l'emplacement:");
-		}
-		else
-		{
-			printf("joueur 1 choisiser l'emplacement:");
-		}
+	{
+		printf("joueur 2 choisiser l'emplacement");
+	}
+	else
+	{
+		printf("joueur 1 choisiser l'emplacement");
+	}
+	
+	printf("\nveuiller entrer 2 nombre de 0 ou 1:");
+	
+	while (!((a >= 0 && a <= 3) && (b >= 0 && b <= 3)))
+	{
+		scanf("%1d%1d", &a, &b);
+		printf("\nveuiller reesayer avec les bonne valeur\n");
+		printf("veuiller entrer 2 nombre de 0 ou 1:");
+		while ((getchar()) != '\n');
+	}
 
-		scanf("%d", &piece_x);
-		scanf("%d", &piece_y);
+	piece_x = a;
+	piece_y = b;
+	
+	enum return_code res = place_piece(game, piece_x, piece_y, p);
+	if(res == SUCCESS){
+		printf("La piece a été placée avec succès.\n");
+	}
+	if(res == POSITION) {
+		printf("Un problème a eu lieu lors du placement\n");
+	}
+	if(res == PIECE) {
+		printf("Un problème a eu lieu avec la piece\n");
+	}
+
 }
 
 
@@ -64,27 +235,38 @@ void choose_emplacement(int nb_tours)
 // fonction permettant l'affichage des pieces dans le terminal
 void affiche_game(board game)
 {
-
-	// creation d'un tableau permettant affichage
-
-	int tab[11][11] = {0};
-
+	int value = 0;
+	// affichage du titre
+	titre();
+	
+	
 	// remplissage du tableau, 0 == aucun element
 	// 1 == premiere characteristique , 2 == seconde characteristique
-
 	for (int i = 0; i < 11; i+=3)
 	{
-
 		for (int y = 0; y < 11; y+=3)
 		{
 			if (is_occupied(game, i/3, y/3))
 			{
+				
+				value = 0;
+				// recupere la piece
 				piece p1 = get_piece(game, i/3, y/3);
 
-				tab[i]    [y]      = (piece_size(p1)) + 1;
-				tab[i]    [y + 1]  = (piece_shape(p1)) + 1;
-				tab[i + 1][y]      = (piece_color(p1)) + 1;
-				tab[i + 1][y + 1]  = (piece_top(p1)) + 1;
+				// recupe la valeur de binaire a base 10 pour la mettre 
+				// dans le tableau, sa marche vraiment
+				
+				value += (piece_size(p1)) * 8;
+				value += (piece_shape(p1)) * 2;
+				value += (piece_color(p1)) / 2;
+				value += (piece_top(p1)) / 8;
+				
+				// met la 
+				played_piece[i/3 * 4 + y/3] = value;
+				ava_piece[value][0] = -1;
+				ava_piece[value][1] = -1;
+				ava_piece[value][2] = -1;
+				ava_piece[value][3] = -1;
 			}
 		}
 	}
@@ -96,46 +278,81 @@ void affiche_game(board game)
 	 sinon si le y > 25 affiche les restant:    0 1 0 1
 												1 0 1 0
 	*/
-
+	
 	// affichage du tableau
-	for (int i = 0; i < 11; i++)
+	printf("|\n|\tPlateau\t      Piece disponible\n|\n");
+	for (int i = 0; i < 20; i++)
 	{
-		for (int y = 0; y < 11; y++)
+		
+		for (int y = 0; y < 40; y++)
 		{
-
-			if (i % 3 == 2)
+			
+			if (y > 2 && y < 20 && i < 17)
 			{
-				printf("-");
-			}
-			else
-			{
-				if (y % 3 == 2)
+				if (i % 4 == 0)
 				{
-					printf("|");
+					printf("\033[9m \033[m");
 				}
 				else
 				{
-					if (tab[i][y] == 0)
+					if (y % 4 == 3)
 					{
-						printf(" ");
+						printf("|");
 					}
 					else
 					{
-						if (tab[i][y] - 1 > 0)
+						if (i % 4 == 2 && y % 4 == 1 && played_piece[((i - 2)) + (y - 5) / 4] != -1)
 						{
-							printf("1");
-						}
+							print_symbol(played_piece[((i - 2)) + (y - 5) / 4]);
+						}	
 						else
 						{
-							printf("0");
+							printf(" ");	
 						}
 					}
 				}
 			}
+		
+			
+			else if (y == 0 || y == 39)
+			{
+				printf("|");
+			}
+			
+			else if ((y >= 11 && y <= 19) || (i >= 11 && y <= 19))
+			{
+				printf(" ");
+			}
+			
+			
+			else if (y > 25 && y < 30 && i < 16)
+			{
+				if (ava_piece[i][y-26] != -1)
+				{
+					printf("%d", ava_piece[i][y-26]);
+				}
+				else
+				{
+					printf(" ");
+				}
+				
+			}
+			else if (y == 31 && i < 16)
+			{
+				print_symbol(i);
+			}
+			
+			else 
+			{
+				printf(" ");
+			}
+			
 		}
 		printf("\n");
 	}
+	printf("\n");
 }
+
 
 
 // fonctions permettant l'affichage du gagnant de la partie
@@ -155,12 +372,18 @@ void is_winner(int nb_tours)
 int main(int args, char **argv){
 
 	// creation d'un nouveau plateau
-	board game = new_game();
+	game = new_game();
 	printf("Un plateau est créé.\n");
-
+	
 	// variable permettant de connaitre qu'elle joueur joue
 	int nb_tours = 0;
-
+	
+	
+	reset_available_piece();
+	
+	
+	affiche_game(game);
+	
 	// boucle principal du jeu
 	while (has_winner(game) == false)
 	{
@@ -171,18 +394,9 @@ int main(int args, char **argv){
 		piece p = choose_piece(nb_tours);
 
 		// apelle de la fonction permettant de choisir emplacement de la piece
-		choose_emplacement(nb_tours);
+		choose_emplacement(nb_tours, p);
 
-		enum return_code res = place_piece(game, piece_x, piece_y, p);
-		if(res == SUCCESS){
-			printf("La piece a été placée avec succès.\n");
-		}
-		if(res == POSITION) {
-			printf("Un problème a eu lieu lors du placement\n");
-		}
-		if(res == PIECE) {
-			printf("Un problème a eu lieu avec la piece\n");
-		}
+		
 
 		// permet de clear le terminal 
 		system("clear");
