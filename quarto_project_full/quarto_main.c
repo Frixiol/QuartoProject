565db37c92f386d5c623c753316e86c9f8527e01
Frixiol <79292185+Frixiol@users.noreply.github.com>
2021-12-16 01:56:54 +0100

    Add files via upload

--- quarto_project_full/quarto_main.c
+++ quarto_project_full/quarto_main.c
@@ -6,6 +6,16 @@
 #include "board.h"
 #include "external.h"
 
+/*
+
+programme crée par: Khalil Mohammad et Prigent Alan
+Inspiré du jeu de Quarto © Gigamic SARL 1991 du concept de Blaise Muller.
+    
+commencer le: 19/11/2021 et finis le: 04/12/2021 
+crée pour l'évaluation de la SAE 1.1 de 1ère année
+Campus 3 de l'IUT Grand Ouest Normandie
+
+*/
 
 enum state { 
 	MENU,
